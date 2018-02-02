ff45aa36dafe12b5c60d9709e5a19bfb7dad1366
foxjazz <foxr4@foxjazz.net>
2018-02-02 16:22:32 -0700

    first or second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {LinksComponent} from "./links/links.component";
+import {PortfolioComponent} from "./portfolio/portfolio.component";
 
+const appRoutes: Routes = [
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'links',      component: LinksComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    LinksComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+        { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
